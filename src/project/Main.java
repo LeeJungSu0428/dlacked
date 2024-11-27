83f753e725d8783201c61517ff74b9c65864f3ff
LeeJungSu0428 <bb337337@naver.com>
2024-11-27 22:56:18 +0900

    Update Main.java
    간단한 구구단 만들기
    배열을 사용한 버전과 그냥 프린트 한 버전

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,38 @@
 package project;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class Main {
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("임창용");
-    }
-
+class GuguDan{
+    int seq;
+    
+    public void gugu(int seq){
+        this.seq = seq;
+        int q = this.seq;
+        int[] result = new int[9];
+        
+        for (int i=1; i<10;i++){
+            int k = i*q;
+            System.out.printf("%d ",k);
+            result[i-1] = k;
+        }
+        System.out.println("");
+        System.out.println("----------------------------------");        
+        System.out.println(Arrays.toString(result));
+        }
+    
+    
+    
+    
+    
 }
+
+
+class Main{
+    public static void main(String[] args){
+    GuguDan Guu = new GuguDan();
+    Scanner sc = new Scanner(System.in);
+    int St = Integer.parseInt(sc.next());
+    Guu.gugu(St);
+    
+    
+    
